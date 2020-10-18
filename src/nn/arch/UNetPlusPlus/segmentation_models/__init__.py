ae1365a80e67c67296f5c96f496887e49b4867b3
swook <wook3024@gmail.com>
2020-10-18 17:59:37 +0900

    remove backbone directory

--- src/nn/arch/UNetPlusPlus/segmentation_models/__init__.py
+++
@@ -1,8 +0,0 @@
-name = "segmentation_models"
-
-from .unet import Unet
-from .nestnet import Nestnet
-from .xnet import Xnet
-from .fpn import FPN
-#from .linknet import Linknet
-from .pspnet import PSPNet
