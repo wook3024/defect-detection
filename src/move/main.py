fe6005033d44fd28ddfac3a9900be5afe9e039d3
wook3024 <wook3024@gmail.com>
2020-11-02 19:44:44 +0900

    add nested+efn.py

---
+++ src/move/main.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import argparse
+from setting import environment, constant
+from util import path, generator
+from nn import nn
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+	print("Name:", gpu.name, " Type:", gpu.device_type)
+
+### gdown https://drive.google.com/uc?id=0B7EVK8r0v71pOXBhSUdJWU1MYUk
+### python main.py --dip=example --tolabel
+### python main.py --dataset=example --dip=example --augmentation=0000
+### python main.py --dataset=example --arch=example --dip=example --gpu --test
+### python main.py --dataset=example --arch=example --dip=example --gpu --train
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--tolabel", help="Preprocess images to create labels (out/tolabel)", action="store_true", default=False)
+	parser.add_argument("--augmentation", help="Dataset augmentation (pass quantity)", type=int)
+	parser.add_argument("--dataset", help="Dataset name", type=str, default=constant.DATASET)
+	parser.add_argument("--train", help="Train", action="store_true", default=False)
+	parser.add_argument("--test", help="Predict", action="store_true", default=False)
+	parser.add_argument("--arch", help="Neural Network architecture", type=str, default=constant.MODEL)
+	parser.add_argument("--dip", help="Method for image processing", type=str, default=constant.IMG_PROCESSING)
+	parser.add_argument("--gpu", help="Enable GPU mode", action="store_true", default=False)
+	parser.add_argument("--count", help="cur_count", type=int, default=7)
+	parser.add_argument("--save_folder", help="save_folder", type=str, default=constant.save_folder)
+	args = parser.parse_args()
+
+	environment.setup(args)
+	exist = lambda x: len(x)>0 and path.exist(path.data(x, mkdir=False))
+
+	if (args.tolabel):
+		generator.tolabel()
+	elif args.dataset is not None and exist(args.dataset):
+		
+		if (args.augmentation):
+			generator.augmentation(args.augmentation)
+		elif (args.train):
+			nn.train()
+		elif (args.test):
+			nn.test()
+	else:
+		print("\n>> Dataset not found\n")
+
+if __name__ == "__main__":
+	main()
